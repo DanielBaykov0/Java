5461dece6509f88b25fd3e41bf683519af8156f3
DanielBaykov0 <99421433+DanielBaykov0@users.noreply.github.com>
2022-03-07 19:43:24 +0200

    Add files via upload

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// the entire line is a statement, but they don't have to be on one line
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                "another" +
+                "still more.");
+
+        int anotherVariable = 5;myVariable--;System.out.println("This is another one.");
+        // white space is the space between words and signs
+
+        // indenting is the idea to type your code, so it is more readable
+        // if we mess up -> Code - Reformat code
+
+    }
+}
