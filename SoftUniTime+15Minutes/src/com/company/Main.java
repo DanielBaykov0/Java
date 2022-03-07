cbfc5885c88a3b7e792dec16f4bb0831f9837cf2
DanielBaykov0 <99421433+DanielBaykov0@users.noreply.github.com>
2022-03-07 19:38:33 +0200

    Add files via upload

---
+++ SoftUniTime+15Minutes/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int hours = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+
+        if (minutes < 45) {
+            System.out.printf("%d:%02d", hours, (minutes + 15));
+        } else if (hours == 23) {
+            System.out.printf("0:%02d", (minutes - 45));
+        } else {
+            System.out.printf("%d:%02d", (hours + 1), (minutes - 45));
+        }
+    }
+}
