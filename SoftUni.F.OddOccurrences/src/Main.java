d25c90704ce072800d11abdc209ff3c1f8ea421e
Daniel Baykov <danibaikov@abv.bg>
2022-10-12 20:09:33 +0300

    Adding Odd Occurrences

---
+++ SoftUni.F.OddOccurrences/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String[] words = sc.nextLine().split(" ");
+
+        LinkedHashMap<String, Integer> counts = new LinkedHashMap<>();
+
+        for (String word : words) {
+            String wordInLowerCase = word.toLowerCase();
+            if (counts.containsKey(wordInLowerCase)) {
+                counts.put(wordInLowerCase, counts.get(wordInLowerCase) + 1);
+            } else {
+                counts.put(wordInLowerCase, 1);
+            }
+        }
+
+        ArrayList<String> odds = new ArrayList<>();
+
+        for (var entry : counts.entrySet()) {
+            if (entry.getValue() % 2 == 1) {
+                odds.add(entry.getKey());
+            }
+        }
+
+        for (int i = 0; i < odds.size(); i++) {
+            System.out.print(odds.get(i));
+            if (i < odds.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+
+    }
+}
