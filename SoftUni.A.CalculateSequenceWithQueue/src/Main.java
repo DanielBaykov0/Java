79f8ae8b87717789c8fbaa85e84eab710d3b88a1
Daniel Baykov <danibaikov@abv.bg>
2022-10-29 20:56:52 +0300

    Adding Calculate Sequence with Queue

---
+++ SoftUni.A.CalculateSequenceWithQueue/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+
+        for (int i = 0; i < 50; i++) {
+            int num = queue.peek();
+            queue.offer(num + 1);
+            queue.offer(2 * num + 1);
+            queue.offer(num + 2);
+
+            System.out.print(queue.poll() + " ");
+
+        }
+    }
+}
