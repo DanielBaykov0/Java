048ae5b89b07c3ea325b0ec38c52fcca650b87c1
Daniel Baykov <danibaikov@abv.bg>
2022-11-12 20:40:37 +0200

    Adding Count Real Numbers

---
+++ SoftUni.A.CountRealNumbers/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] values = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble).toArray();
+
+        Map<Double, Integer> valuesWithOccurrences = new LinkedHashMap<>();
+
+        for (double value : values) {
+            if (!valuesWithOccurrences.containsKey(value)) {
+                valuesWithOccurrences.put(value, 1);
+            } else {
+                valuesWithOccurrences.put(value, valuesWithOccurrences.get(value) + 1);
+            }
+        }
+
+        for (Double key : valuesWithOccurrences.keySet()) {
+            System.out.println(String.format("%.1f -> %d", key, valuesWithOccurrences.get(key)));
+        }
+    }
+}
