dffc6d359b626f603771b6fb007afdcbfbcc89cd
Daniel Baykov <danibaikov@abv.bg>
2022-11-20 21:09:33 +0200

    Adding Library

---
+++ Java Advanced/Iterators and Comparators - Lab/Library/src/Book.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+
+    String title;
+    int year;
+    List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    private void setAuthors(String... authors) {
+        if (authors.length == 0) {
+            this.authors = new ArrayList<>();
+        } else {
+            this.authors = new ArrayList<>(Arrays.asList(authors));
+        }
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", authors=" + authors +
+                '}';
+    }
+}
