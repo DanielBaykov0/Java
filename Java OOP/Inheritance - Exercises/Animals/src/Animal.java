332d13f055fd38782851dd1958b2a2900fed40fd
Daniel Baykov <danibaikov@abv.bg>
2022-11-25 12:38:57 +0200

    Adding Animals

---
+++ Java OOP/Inheritance - Exercises/Animals/src/Animal.java
@@ -0,0 +1,54 @@
+public abstract class Animal {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animal(String name, int age, String gender) {
+        this.setName(name);
+        this.setAge(age);
+        this.setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        if (gender == null || gender.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.gender = gender;
+    }
+
+    public abstract String produceSound();
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s %d %s%n%s",
+                this.getClass().getSimpleName(),
+                this.getName(), this.getAge(), this.getGender(), this.produceSound());
+    }
+}
